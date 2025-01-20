50c81f58200cc2824926da6271dbcf4c66e6cddc
Atsushi <atsushi@moongift.jp>
2025-01-20 15:21:42 +0900

    Add `generateToken` method to `Client` struct to generate an API request token
    * Concatenate `userId` and `apiKey` in `generateToken` method
    * Generate a SHA256 hash of the concatenated string
    * Base64 encode the hash string
    * Remove unnecessary conversion of hash to lowercase string

--- client.go
+++ client.go
@@ -3,7 +3,6 @@ package blastenginego
 import (
 	"crypto/sha256"
 	"encoding/base64"
-	"strings"
 )
 
 type Client struct {
@@ -24,11 +23,8 @@ func (c *Client) generateToken() string {
 	// Generate SHA256 hash
 	hash := sha256.Sum256([]byte(concatenated))
 
-	// Convert hash to lowercase string
-	hashString := strings.ToLower(base64.StdEncoding.EncodeToString(hash[:]))
-
-	// Base64 encode the lowercase hash string
-	token := base64.StdEncoding.EncodeToString([]byte(hashString))
+	// Base64 encode the hash string
+	token := base64.StdEncoding.EncodeToString(hash[:])
 
 	return token
 }
